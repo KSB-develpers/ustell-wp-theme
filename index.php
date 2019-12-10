863bb452d9dc187d3e7f201f58cdf161d62d4c5c
KSB <green@ksb-sendai.co.jp>
2019-12-10 21:52:25 +0900

    add index.php へのコメント追加

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * あすてる用のwordpress独自テーマです。
+ *
+ * これは、あすてるのwebサイト作成をするためにKSBが独自で開発をしているwordpressのテーマファイル群です。
+ * 文字のサイズ等の基本的なスタイルは style.css から変更をすることが可能になっています。
+ * その他不明な点があれば、加藤涼太郎(green)に尋ねてください。
+ *
+ * @package WordPress
+ * @subpackage UstellOrigen
+ * @version 1.0.0
+ */
+get_header();
+?>
