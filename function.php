1d4bd10641de2fa1d83f423cb0fb6a5d17dce338
KSB <green@ksb-sendai.co.jp>
2019-12-11 00:37:02 +0900

    add モバイル用のヘッダー一旦完成

---
+++ function.php
@@ -0,0 +1,4 @@
+<?php
+// マークアップの指定 => html5
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallerey', 'caption' ) );
+?>
---
+++ header.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * あすてるのテーマ用のヘッダーのファイル
+ *
+ * @package WordPress
+ * @subpackage UstellOrigen
+ * @version 1.0.0
+ */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charser="<?php bloginfo( 'charset' )?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <!-- 同一ディレクトリにあるcssを読み込む -->
+        <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" />
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
+        <?php wp_body_open(); ?>
+        <div id="page-top" class="page-header">
+            <div class="header-logo">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ustell_header_logo.png" />
+            </div>
+            <div class="header-nav">
+                <a class="nav">ホーム</a>
+                <a class="nav">サービス</a>
+                <a class="nav">ニュースリリース</a>
+                <a class="nav">私たちの思い</a>
+                <a class="nav">会社概要</a>
+                <div class="nav-contact">
+                    <a>お問い合わせ</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
